bb09776c031708b00fd9e5156ed2f607ae8b59b7
deepak <sidhudeep9292@gmail.com>
2024-12-12 22:22:30 +0000

    update

--- org/apps/todo-app/src/app/app.spec.tsx
+++ org/apps/todo-app/src/app/app.spec.tsx
@@ -1,15 +1,24 @@
+import React from 'react';
 import { render } from '@testing-library/react';
-
-import App from './app';
+import { RecoilRoot } from 'recoil';
+import App from './App';
 
 describe('App', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<App />);
+    const { baseElement } = render(
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    );
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
+    const { getByText } = render(
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    );
     expect(getByText(/Welcome todo-app/gi)).toBeTruthy();
   });
 });
